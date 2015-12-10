4171a2f8b56e4abda050d46a5f56a7059f26cb7c
Carmelocrlpz <ccr376@gmail.com>
2015-12-09 18:34:58 -0600

    Ejemplo de apuntadores con char

---
+++ EjeApuntadores.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+char cadena [10] = {"hola mundo"};
+int arreglo [10] = {0,1,2,3,4,5,6,7,8,9};
+char *cadena1 = "hola mundo mas largo";
+char *meses[3] = {"enero","frebrero","marzo"};
+int i;
+
+int main()
+{
+	for (int i = 0; i < 3; ++i)
+	
+	printf("%c %c %c \n",cadena1[0],cadena1[1],cadena1[2] );
+
+	system("pause");
+}
